dc1d700a90f91a0db7a9b3292c6d77b6f79ae808
Coen Elferink <coen_elferink@hotmail.com>
2022-04-24 10:50:18 +0200

    Tasks basic layout

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,6 +3,7 @@ const makeElement = require('../helpers/makeElement.js')
 export function tasksShown(tasks) {
     const list = makeElement('ul',['task-list'])
     for (let task of tasks) {
+        
         const item = makeElement('li', ['task-item'], task.title)
         list.appendChild(item)
     }
