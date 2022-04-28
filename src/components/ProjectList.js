388956e3470679df9276572bb64aaf871de9e10e
Coen Elferink <coen_elferink@hotmail.com>
2022-04-28 11:21:02 +0200

    Add amount of tasks in project list

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,17 +6,33 @@ export function projectList(projects, projectList) {
     let misc
     for (let project of projects) {
         if (project.title !== '_no-project') {
-            const p = makeElement('li', ['project-link'], project.title, { 'tabindex': '0' })
-            projectList.appendChild(p)
-            p.addEventListener('click', () => createTaskView('p', project))
+            const li = makeElement('li',['project-link'], null, {'tabindex': '0'})
+
+            const p = makeElement('span', null, project.title)
+            li.appendChild(p)
+
+            if (project.tasks.length) {
+                const num = makeElement('span',['project-amount'], project.tasks.length)
+                li.appendChild = num
+            }
+            projectList.appendChild(li)
+            li.addEventListener('click', () => createTaskView('p', project))
+
         } else {
             misc = project
         }
     }
+    
     projectList.appendChild(makeElement('hr'))
-    const miscProject = makeElement('li', ['project-link', 'misc-project-link'], 'Miscellaneous tasks', { 'tabindex': '0' })
-    projectList.appendChild(miscProject)
-    miscProject.addEventListener('click', () => createTaskView('p', misc))
+    const li = makeElement('li',['project-link'], null, {'tabindex': '0'})
+    const miscProject = makeElement('span', null, 'Miscellaneous tasks')
+    li.appendChild(miscProject)
+    
+    const miscNum = makeElement('span',['project-amount'], misc.tasks.length)
+    li.appendChild(miscNum)
+
+    projectList.appendChild(li)
+    li.addEventListener('click', () => createTaskView('p', misc))
 
     return projectList
 }
